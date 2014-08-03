0689f447a9633aebe1f835f541ed29bad0f46c90
NeverWise <matteopomi@libero.it>
2014-08-03 13:43:57 +0200

    Version 1.0.2 - Small improvements. - Adapted to the xbmc repository guidelines. - New icon.

--- default.py
+++ default.py
@@ -4,5 +4,5 @@ from neverwise import Util
 
 # http://resetdiretta.ns0.it:8000 # URL alternativo.
 # Ho tentato di inserire il nome della radio tra le informazioni della canzone ma dallo stream ricevo le informazioni della canzone corrente che cancellano il nome della radio.
-li = Util.createListItem('Reset Radio', os.path.dirname(os.path.abspath(__file__)) + '/icon.png', '', 'music', { 'title' : 'Reset Radio' })
+li = Util.createListItem(Util._addonName, thumbnailImage = '{0}/icon.png'.format(os.path.dirname(os.path.abspath(__file__))), streamtype = 'music', infolabels = { 'title' : Util._addonName })
 xbmc.Player(xbmc.PLAYER_CORE_PAPLAYER).play('http://resetradiolive.ns0.it:8000', li)
